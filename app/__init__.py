27f31cfa4f997f6830137ce483f3a26d8d137d34
Amukozoberit <mwasheb@gmail.com>
2021-08-20 15:14:57 +0300

    regitered blueprint and defined

--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,16 @@ def create_app(config_name):
     config[config_name].init(app)
     bootstrap.init_app(app)
 
+
+    #register blueprint
+    from . main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    from . auth import main as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/auth')
+
+
+    from .request import configure_request
+    configure_request(app)
     return app
