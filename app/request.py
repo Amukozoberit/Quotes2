ccc4d0d15f342a2c4a78e58214593320501a2f5e
Amukozoberit <mwasheb@gmail.com>
2021-08-21 15:07:18 +0300

    configured url

--- app/request.py
+++ app/request.py
@@ -1,6 +1,13 @@
 import urllib,json
+from app import app
+
+
+
+
+
+base_url=app.config['QUOTES_URL']
 def get_quotes():
-    quotes_url='http://quotes.stormconsultancy.co.uk/random.json'
+    quotes_url='base_url'
     with urllib.request.urlopen(quotes_url) as url:
         quotes_data=url.read()
         quotes_data_response=json.loads(quotes_data)
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ import os
 class Config:
   SECRET_KEY='rE\x1eKQ\xa0\x80\xee\xa2\xfcf{\xb9Ki&'
   SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://mwashe:github2122@localhost/quotesa'
+  QUOTES_URL='http://quotes.stormconsultancy.co.uk/random.json'
 class DevelopmentConfig(Config):
     DEBUG=True
 class ProductionConfig(Config):
