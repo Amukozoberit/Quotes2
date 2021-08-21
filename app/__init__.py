203faaa9f9332fac727a4d7d6d645a8ddabf1fd4
Amukozoberit <mwasheb@gmail.com>
2021-08-21 10:32:44 +0300

    added migrate or imported

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
+from logging import Manager
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import  SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate,MigrateCommand
+
 bootstrap=Bootstrap()
 db=SQLAlchemy()
 login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
+
 def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config_options[config_name])
@@ -21,10 +27,11 @@ def create_app(config_name):
     from . auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-  
+    
     db.init_app(app)
     login_manager.init_app(app)
-
+  
+   
     # from .request import configure_request
     # configure_request(app)
     return app
--- manage.py
+++ manage.py
@@ -1,13 +1,18 @@
 import os
 
+from flask_migrate import Migrate, MigrateCommand
 
-from app import create_app
+
+from app import create_app,db
 from flask_script import Manager,Server
 
 
 
 app=create_app('development')
 manager=Manager(app)
+manager.add_command('server',Server)
+migrate=Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 
 if __name__=='__main__':
