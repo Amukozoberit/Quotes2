fc8f45c9e8dd22cfa1b65f1e72979495a1309e49
Amukozoberit <mwasheb@gmail.com>
2021-08-22 09:13:06 +0300

    injcted permisions globally for templates

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from ..models  import Permission
 main=Blueprint('main',__name__)
 
 
@@ -8,3 +9,6 @@ from . import views,errors
 
 
 
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
