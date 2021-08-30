bbb92ea639c02e13dca7d2d9bed5e20dcd1a3612
Amukozoberit <mwasheb@gmail.com>
2021-08-30 11:27:44 +0300

    import uploads

--- app/__init__.py
+++ app/__init__.py
@@ -6,20 +6,25 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate,MigrateCommand
-
-
+from flask_mail import Mail
+import requests
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 bootstrap=Bootstrap()
 db=SQLAlchemy(session_options={"autoflush": False})
 login_manager=LoginManager()
 login_manager.session_protection='strong'
 login_manager.login_view='auth.login'
 security=Security()
+mail = Mail()
+photos=UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config_options[config_name])
    
     bootstrap.init_app(app)
+    mail.init_app(app)
+    configure_uploads(app,photos)
 
 
     #register blueprint
@@ -34,7 +39,7 @@ def create_app(config_name):
     app.register_blueprint(posts_blueprint,url_prefix='/blogposts')
 
     from . moderator import moderator as moderator_blueprint
-    app.register_blueprint(moderator_blueprint,url_prefix='/blogposts')
+    app.register_blueprint(moderator_blueprint,url_prefix='/moderate')
 
     db.init_app(app)
     login_manager.init_app(app)
