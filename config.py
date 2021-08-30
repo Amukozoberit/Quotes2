db92c7921b875f4e51393fc82cefdec1d218daea
Amukozoberit <mwasheb@gmail.com>
2021-08-30 11:27:25 +0300

    addedprocfile

--- config.py
+++ config.py
@@ -4,9 +4,19 @@ import os
 
 
 class Config:
+  # SECRET_KEY=os.getenv.get('SECRET_KEY')
   SECRET_KEY='rE\x1eKQ\xa0\x80\xee\xa2\xfcf{\xb9Ki&'
   SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://mwashe:github2122@localhost/quotesa'
   QUOTES_URL='http://quotes.stormconsultancy.co.uk/random.json'
+#   SECRET_KEY=os.environ.get('SECRET_KEY')
+  MAIL_SERVER = 'smtp.gmail.com'
+  MAIL_PORT = 465
+  MAIL_USE_TLS = False
+  MAIL_USE_SSL = True
+  MAIL_USERNAME ='mwasheberit@gmail.com'
+  MAIL_PASSWORD ='github2122'
+  UPLOADED_PHOTOS_DEST='app/static/photos'
+
 class DevelopmentConfig(Config):
     DEBUG=True
 class ProductionConfig(Config):
